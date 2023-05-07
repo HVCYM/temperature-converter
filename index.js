60da769532fdfeb7a546fd7bb503586e4d355d92
github-himanshu-yadav <himanshuyadav20@hotmail.com>
2023-05-07 11:56:20 +0530

    main

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+const input = document.querySelector('.main__container-input');
+const output = document.querySelector('.output');
+const option = document.querySelector('.main__container-options');
+const submitButton = document.querySelector('.main__container-submit');
+
+submitButton.addEventListener('click', (event) => {
+   let ans = 0;
+   const data = input.value;
+   console.log(data);
+   if(option.value) ans = data * 9 / 5 + 32;
+   else ans = (data - 32) * 5 / 9;
+   console.log(ans);
+   output.value = ans;
+});
